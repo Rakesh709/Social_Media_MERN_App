430686d500e264febdc83142cee1929c41e4f92e
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-01-10 13:20:54 +0530

    User Routes Setup-Read

---
+++ server/controllers/users.js
@@ -0,0 +1,35 @@
+import User from "../modles/User";
+
+//Read
+export const getUser = async (req, res)=>{
+    try{
+        const {id} =  req.params;
+        const user = await User.findById(id);
+        res.status(200).json(user);
+
+    }catch(err){
+        res.status(404).json({message: err.message});
+    }
+}
+
+export const getUserFriends = async(req,res)=>{
+    try{
+    const {id} =  req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promis.all(
+        user.friends.map((id) => User.findById(id))
+    );
+
+    const formattedFriends=friends.mp(
+       ({_id,firstName,lastName, occupation,location,picturePath}) =>{
+        return {_id,firstName,lastName, occupation,location,picturePath};
+       }
+    );
+    res.status(200).json(formattedFriends);
+
+    }catch(err){
+        res.status(404).json({message:err.message});
+    }
+    
+}
