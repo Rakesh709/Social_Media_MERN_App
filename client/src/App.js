676591ee684b99dcb37a5856caf60d764c48c55e
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-01-17 11:01:39 +0530

    Theme configure to Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,29 @@ import HomePage from "scenes/homePage";
 //the above is used for reference the convience way to do 
 import loginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
-
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline,ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/materil/style";
+import { themeSettings } from "./theme";
 
 
 
 function App() {
+  const mode = useSelector((state)=>state.mode);
+  const theme = useMemo(()=>createTheme(themeSettings(mode)),[mode]);
+
+
   return (
     <div className="app">
       <BrowserRouter>
+      <ThemeProvider theme={theme}>
         <Routes>
           <Route  path="/" element={<loginPage/>}  />
           <Route  path="/home" element={<HomePage/>}  />
           <Route  path="/profile/:userId" element={<ProfilePage/>}  />
         </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
