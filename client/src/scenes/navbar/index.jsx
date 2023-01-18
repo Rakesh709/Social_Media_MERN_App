457b98c30f54ad10e32d9ada2845255e126b8ad5
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-01-18 11:40:16 +0530

    setup & configure Navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -23,7 +23,27 @@ import {
 } from "@mui/icons-material";
 
 
+import { useDispatch, useSelector } from "react-redux";
+import { setMode,setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
+
+
+
 const Nabar=()=>{
+
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state)=> state.user);
+    const isNotMobileScreens= useMediaQuery("(min-width:1000px)");
+
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.Light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+
     return(
         <div>Nabar</div>
     )
