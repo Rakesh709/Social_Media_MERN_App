234d62ec0e5ddafbc570a0b2ee21d7cbf896aba1
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-01-20 20:43:13 +0530

    Creating Navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -84,8 +84,28 @@ const Nabar=()=>{
                         <DarkMode sx={{fontSize:"25px"}} />
                     ):(
                         <LightMode sx={{ color:dark ,fontSize:"25px"}}/>
+                        //{/* using the redux to change dark and light */}
                     )}
                 </IconButton>
+                <Message sx={{fontSize:"25px"}}/>
+                <Notification sx={{fontSize:"25px"}}/>
+                <Help sx={{fontSize:"25px"}}/>
+                <FormControl variant="standard" value={fullName}>
+                    <Select
+                    value={fullName}
+                    sx={{
+                        backgroundColor:neutralLight,
+                        width:"150px",
+                        borderRadius:"0.25rem",
+                        p:"1.25rem 1rem",
+                        
+
+                    }}
+                    >
+
+                    </Select>
+                </FormControl>
+
             </FlexBetween>
             ) : (
                 <IconButton></IconButton>)}
